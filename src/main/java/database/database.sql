0b26bdf3ffc79e0bb18e7ead9c42521388d39289
nurcan <nurcannafii@outlook.com>
2024-11-05 15:52:27 +0400

    database

--- src/main/java/database/database.sql
+++ src/main/java/database/database.sql
@@ -36,20 +36,29 @@ CREATE TABLE comments
      post_id    INT REFERENCES posts(id)
   );
 
-CREATE TABLE likes
+CREATE TABLE post_likes
   (
      id         SERIAL PRIMARY KEY,
      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
      updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
      created_by INT NOT NULL REFERENCES users(id),
      updated_by INT REFERENCES users(id),
-     user_id    INT UNIQUE REFERENCES users(id),
-     post_id    INT UNIQUE REFERENCES posts(id),
-     comment_id INT UNIQUE REFERENCES comments(id),
-     CHECK (post_id IS NOT NULL AND comment_id IS NULL OR post_id IS NULL AND
-     comment_id IS NOT NULL)
+     user_id    INT NOT NULL REFERENCES users(id),
+     post_id    INT NOT NULL REFERENCES posts(id),
+     UNIQUE(user_id, post_id)
   );
 
+CREATE TABLE comment_likes
+  (
+     id         SERIAL PRIMARY KEY,
+     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+     created_by INT NOT NULL REFERENCES users(id),
+     updated_by INT REFERENCES users(id),
+     user_id    INT NOT NULL REFERENCES users(id),
+     comment_id INT NOT NULL REFERENCES comments(id),
+     UNIQUE(user_id, comment_id)
+  );
 
 SELECT p.id        post_id,
        p.title     post_title,
@@ -63,7 +72,7 @@ FROM   posts p
        LEFT JOIN users u1
               ON p.created_by = u1.id
        LEFT JOIN users u2
-              ON p.updated_by = u1.id;
+              ON p.updated_by = u2.id;
 
 SELECT c.id        comment_id,
        c.created_at,
@@ -83,7 +92,7 @@ SELECT p.id        post_id,
        p.title,
        Count(l.id) like_count
 FROM   posts p
-       LEFT JOIN likes l
+       LEFT JOIN post_likes l
               ON p.id = l.post_id
 GROUP  BY p.id;
 
@@ -93,7 +102,7 @@ SELECT c.id        comment_id,
 FROM   comments c
        LEFT JOIN posts p
               ON c.post_id = p.id
-       LEFT JOIN likes l
+       LEFT JOIN comment_likes l
               ON c.id = l.comment_id
 GROUP  BY c.id,
           p.id;
@@ -102,13 +111,11 @@ SELECT u.username,
        p.title   liked_post,
        c.content liked_comment
 FROM   users u
-       LEFT JOIN likes l
-              ON u.id = l.user_id
+       LEFT JOIN post_likes pl
+              ON u.id = pl.user_id
+       LEFT JOIN comment_likes cl
+              ON u.id = cl.id
        LEFT JOIN posts p
-              ON l.post_id = p.id
+              ON pl.post_id = p.id
        LEFT JOIN comments c
-              ON l.comment_id = c.id;
-
-
-
-
+              ON cl.comment_id = c.user_id;
